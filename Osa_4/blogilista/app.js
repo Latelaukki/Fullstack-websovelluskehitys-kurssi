6d688aa8ec026b4a43335e4dddb1b4e6812662a1
Lauri Aaltonen <aaltonenlauri1@gmail.com>
2022-02-16 14:49:31 +0200

    4.2

---
+++ Osa_4/blogilista/app.js
@@ -0,0 +1,25 @@
+const config = require('./utils/config')
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const notesRouter = require('./controllers/blogs')
+const middleware = require('./utils/middleware')
+const logger = require('./utils/logger')
+const mongoose = require('mongoose')
+
+logger.info('connecting to', config.MONGODB_URI)
+
+mongoose.connect(config.MONGODB_URI)
+  .then(() => {
+    console.log('connected to MongoDB')
+  })
+  .catch((error) => {
+    console.log('error connecting to MongoDB:', error.message)
+  })
+
+app.use(cors())
+app.use(express.json())
+
+app.use('/api/blogs', notesRouter)
+
+module.exports = app
--- Osa_4/blogilista/index.js
+++ Osa_4/blogilista/index.js
@@ -1,44 +1,9 @@
+const app = require('./app')
 const http = require('http')
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const mongoose = require('mongoose')
-const Blog = require('./models/blog')
 const config = require('./utils/config')
 const logger = require('./utils/logger')
 
 const server = http.createServer(app)
-
-console.log('connecting to', config.MONGODB_URI)
-
-mongoose.connect(config.MONGODB_URI)
-  .then(() => {
-    console.log('connected to MongoDB')
-  })
-  .catch((error) => {
-    console.log('error connecting to MongoDB:', error.message)
-  })
-
-app.use(cors())
-app.use(express.json())
-
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
 
 server.listen(config.PORT, () => {
   logger.info(`Server running on port ${config.PORT}`)
