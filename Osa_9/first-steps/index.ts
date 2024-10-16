22f1964e31ba490a8bf0784a586b21e8db7c6f91
Lauri Aaltonen <aaltonenlauri1@gmail.com>
2024-10-16 20:49:46 +0300

    9.7

--- Osa_9/first-steps/index.ts
+++ Osa_9/first-steps/index.ts
@@ -4,6 +4,8 @@ import { calculateExercises } from './exerciseCalculator';
 
 const app = express();
 
+app.use(express.json());
+
 app.get('/hello', (_req, res) => {
     res.send('Hello Full Stack!');
 });
@@ -23,11 +25,23 @@ app.get('/bmi', (req, res) => {
 });
 
 app.post('/exercises', (req, res) => {
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-  const { target, daily_exercises } = req.body;
+  
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+  if (Object.keys(req.body).length < 2)  {
+    res.status(400).send({ error: "parameters missing"});
+  }
+ 
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-explicit-any
+  const { target, daily_exercises } : any = req.body;
 
-  const result = calculateExercises(target, daily_exercises);
-  res.send({ result });
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+  if (isNaN(Number(target)) || daily_exercises.some(isNaN)) {
+    res.status(400).send({ error: "malformatted parameters" });
+  } else {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+    const result = calculateExercises(Number(target), daily_exercises);
+    res.send({ result });
+  }
 });
 
 const PORT = 3003;
